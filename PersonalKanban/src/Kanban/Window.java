479fbbdf301c96c1b65d680ade6753fc9663579d
Ro0 <Ro0@Ro0-PC>
2014-05-14 09:05:09 -0500

    modificaciones

--- PersonalKanban/src/Kanban/Window.java
+++ PersonalKanban/src/Kanban/Window.java
@@ -8,7 +8,6 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
-
 import javax.swing.JApplet;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
@@ -34,7 +33,7 @@ public class Window extends JFrame{
 		gc.gridx=1;
 		gc.gridy=0;
 		JTextField txtitle= new JTextField();
-		txtitle.setPreferredSize(new Dimension(130,25));
+		txtitle.setPreferredSize(new Dimension(100,25));
 		add(txtitle,gc);
 
 		gc.gridx=0;
@@ -43,7 +42,7 @@ public class Window extends JFrame{
 		gc.gridx=1;
 		gc.gridy=2;
 		JTextField txdesc= new JTextField();
-		txdesc.setPreferredSize(new Dimension(130,200));
+		txdesc.setPreferredSize(new Dimension(100,100));
 		add(txdesc,gc);
 
 		gc.gridx=0;
@@ -51,7 +50,7 @@ public class Window extends JFrame{
 		add(new JLabel("Estado: "),gc);
 		gc.gridx=1;
 		gc.gridy=3;
-		String [] categories = {"","BACKLOG", "DO_TO", "IN_PROGRESS", "DONE"};
+		String [] categories = {"Elige un estado","BACKLOG", "DO_TO", "IN_PROGRESS", "DONE"};
 		JComboBox cbstate= new JComboBox<String>(categories);
 		cbstate.setPreferredSize(new Dimension(130,25));
 		add(cbstate,gc);
@@ -62,7 +61,7 @@ public class Window extends JFrame{
 		gc.gridx=1;
 		gc.gridy=4;
 		JTextField txcatego= new JTextField();
-		txcatego.setPreferredSize(new Dimension(130,25));
+		txcatego.setPreferredSize(new Dimension(100,25));
 		add(txcatego,gc);
 
 		gc.gridx=0;
@@ -70,9 +69,9 @@ public class Window extends JFrame{
 		add(new JLabel("Prioridad: "),gc);
 		gc.gridx=1;
 		gc.gridy=5;
-		Integer [] priorities = {1,2,3,4,5,6,7,8,9,10};
+		Integer [] priorities = {0,1,2,3,4,5};
 		JComboBox priority= new JComboBox<Integer>(priorities);
-		priority.setPreferredSize(new Dimension(130,25));
+		priority.setPreferredSize(new Dimension(100,25));
 		add(priority,gc);
 
 		gc.gridx=0;
@@ -81,28 +80,26 @@ public class Window extends JFrame{
 		gc.gridx=1;
 		gc.gridy=6;
 		JTextField txprop= new JTextField();
-		txprop.setPreferredSize(new Dimension(130,25));
+		txprop.setPreferredSize(new Dimension(100,25));
 		add(txprop,gc);
 
 		gc.gridx=0;
-		gc.gridy=7;
+		gc.gridy=10;
 		btnAdd = new JButton ("Add");
 		btnAdd.addActionListener(new ConfirmationAction());
 		add(btnAdd,gc);
-		gc.gridx=1;
-		gc.gridy=7;
+		gc.gridx=2;
+		gc.gridy=10;
 		btnCan = new JButton ("Cancel");
 		btnCan.addActionListener(new CancelAction());
 		add(btnCan,gc);
 
 
 	}
-
 	private void addListener() {
 		btnAdd.addActionListener(new ConfirmationAction());
 		btnCan.addActionListener(new CancelAction());
 	}
-
 	private class ConfirmationAction implements ActionListener
 	{
 		public void actionPerformed(ActionEvent e)
@@ -110,8 +107,6 @@ public class Window extends JFrame{
 			JOptionPane.showMessageDialog(null,"Confirmed");
 		}
 	}
-
-
 	private class CancelAction implements ActionListener
 	{
 		public void actionPerformed(ActionEvent e)
@@ -119,12 +114,11 @@ public class Window extends JFrame{
 			dispose();
 		}
 	}
-
 	public static void main(String[] args){
 
 		JFrame labelFrame2=new Window("Agregar");
 		labelFrame2.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		labelFrame2.setSize(320,500);
+		labelFrame2.setSize(320,350);
 		labelFrame2.setVisible(true);
 	}
 }
